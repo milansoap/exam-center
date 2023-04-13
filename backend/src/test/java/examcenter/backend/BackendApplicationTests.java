3a20dcd525114a10cee369ce9804091b2619cdfe
milansoap <milan.seid123@gmail.com>
2023-04-13 12:33:30 +0200

    initial backend commit

---
+++ backend/src/test/java/examcenter/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package examcenter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
